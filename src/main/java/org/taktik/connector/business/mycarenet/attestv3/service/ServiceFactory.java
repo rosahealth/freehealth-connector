636dba882e3e68f8f93ae44c774932bd83b4365b
Alina Zagan <azagan.meyvn@gmail.com>
2023-08-25 17:56:25 +0300

    use config to determine eAttest v3 url

--- src/main/java/org/taktik/connector/business/mycarenet/attestv3/service/ServiceFactory.java
+++ src/main/java/org/taktik/connector/business/mycarenet/attestv3/service/ServiceFactory.java
@@ -1,5 +1,7 @@
 package org.taktik.connector.business.mycarenet.attestv3.service;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.taktik.connector.technical.config.ConfigFactory;
 import org.taktik.connector.technical.config.Configuration;
 import org.taktik.connector.technical.exception.TechnicalConnectorException;
@@ -9,12 +11,14 @@ import org.taktik.connector.technical.ws.domain.TokenType;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.lang.Validate;
+import org.taktik.connector.technical.ws.impl.AbstractWsSender;
 
 public final class ServiceFactory {
    private static final String PROP_ENDPOINT_ATTEST_V3 = "endpoint.eattestv3";
    public static final String PROP_MESSAGE_PROTOCOLE_SCHEMA_VERSION_V3 = "mycarenet.attest.v3.message.protocole.schema.version";
    private static final List<String> expectedProps = new ArrayList();
    private static final Configuration config;
+   private static final Log log = LogFactory.getLog(AbstractWsSender.class);
 
    private ServiceFactory() {
    }
@@ -22,7 +26,11 @@ public final class ServiceFactory {
    public static GenericRequest getAttestPort(SAMLToken token, String soapAction) throws TechnicalConnectorException {
       Validate.notNull(token, "Required parameter SAMLToken is null.");
       String endPoint = config.getProperty("endpoint.eattestv3", "https://services-acpt.ehealth.fgov.be/MyCareNet/eAttest/v3");
-      return (new GenericRequest()).setEndpoint("https://services-acpt.ehealth.fgov.be/MyCareNet/eAttest/v3").setCredential(token, TokenType.SAML).setSoapAction(soapAction).addDefaulHandlerChain();
+
+      log.info("--------------------endpoint--------------------------");
+      log.info(endPoint);
+
+      return (new GenericRequest()).setEndpoint(endPoint).setCredential(token, TokenType.SAML).setSoapAction(soapAction).addDefaulHandlerChain();
    }
 
    static {
