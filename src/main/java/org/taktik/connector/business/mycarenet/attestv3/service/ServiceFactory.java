46c458699035fae6d4ea153b67b0c42703d0c5d7
adrian <>
2023-12-19 13:56:48 +0200

    remove logging

--- src/main/java/org/taktik/connector/business/mycarenet/attestv3/service/ServiceFactory.java
+++ src/main/java/org/taktik/connector/business/mycarenet/attestv3/service/ServiceFactory.java
@@ -1,7 +1,5 @@
 package org.taktik.connector.business.mycarenet.attestv3.service;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.taktik.connector.technical.config.ConfigFactory;
 import org.taktik.connector.technical.config.Configuration;
 import org.taktik.connector.technical.exception.TechnicalConnectorException;
@@ -11,14 +9,12 @@ import org.taktik.connector.technical.ws.domain.TokenType;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.lang.Validate;
-import org.taktik.connector.technical.ws.impl.AbstractWsSender;
 
 public final class ServiceFactory {
    private static final String PROP_ENDPOINT_ATTEST_V3 = "endpoint.eattestv3";
    public static final String PROP_MESSAGE_PROTOCOLE_SCHEMA_VERSION_V3 = "mycarenet.attest.v3.message.protocole.schema.version";
    private static final List<String> expectedProps = new ArrayList();
    private static final Configuration config;
-   private static final Log log = LogFactory.getLog(AbstractWsSender.class);
 
    private ServiceFactory() {
    }
@@ -26,10 +22,6 @@ public final class ServiceFactory {
    public static GenericRequest getAttestPort(SAMLToken token, String soapAction) throws TechnicalConnectorException {
       Validate.notNull(token, "Required parameter SAMLToken is null.");
       String endPoint = config.getProperty("endpoint.eattestv3", "$uddi{uddi:ehealth-fgov-be:business:mycareneteattest:v3}");
-
-      log.info("--------------------new endpoint--------------------------");
-      log.info(endPoint);
-
       return (new GenericRequest()).setEndpoint(endPoint).setCredential(token, TokenType.SAML).setSoapAction(soapAction).addDefaulHandlerChain();
    }
 
